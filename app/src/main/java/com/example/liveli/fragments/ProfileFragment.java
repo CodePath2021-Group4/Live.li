7039d3c5eb412f5e392d7e4645caf1ed22e66b18
julesy <julesssie.dev@gmail.com>
2021-11-22 00:33:52 -0500

    updated profile fragment with functing views

--- app/src/main/java/com/example/liveli/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/liveli/fragments/ProfileFragment.java
@@ -1,14 +1,31 @@
 package com.example.liveli.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.liveli.LoginActivity;
+import com.example.liveli.Profile;
 import com.example.liveli.R;
+import com.example.liveli.parseobjects.UserProfile;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +33,7 @@ import com.example.liveli.R;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+    public static final String TAG = "HomeFragment";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,5 +80,53 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        ImageView ivPostImage = view.findViewById(R.id.ivProfilePic);
+
+        loadProfileImage(ivPostImage, currentUser);
+
+        TextView tvProfileName = view.findViewById(R.id.tvUsername);
+        tvProfileName.setText(currentUser.getUsername());
+
+        ImageButton ibExit =  view.findViewById(R.id.ibExit);
+        ibExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ParseUser.getCurrentUser() != null) {
+                    ParseUser.logOut();
+
+                    logout();
+                }
+            }
+        });
+
+    }
+
+    private void logout() {
+        Intent i =  new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
+    }
+
+    //--user defined
+    private void loadProfileImage(ImageView ivPostImage, ParseUser currentUser) {
+        ParseQuery<UserProfile> query = ParseQuery.getQuery(UserProfile.class);
+        query.whereEqualTo(UserProfile.KEY_USER, currentUser);
+        query.findInBackground(new FindCallback<UserProfile>() {
+            @Override
+            public void done(List<UserProfile> objects, ParseException e) {
+                if ( e != null) {
+                    Log.e(TAG, "Error Loading Profile Image", e);
+                } else {
+                    Glide.with(getContext()).load(objects.get(0).getImage().getUrl()).into(ivPostImage);
+                }
+            }
+        });
     }
 }
