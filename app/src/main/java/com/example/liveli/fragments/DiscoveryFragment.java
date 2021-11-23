c074a68f4a583c5d9c2faf9cc6420215689c2913
MateoMicciche <mateo.micciche@gmail.com>
2021-11-23 12:41:46 -0500

    Initial commit for this branch. Implemented basic version of search bar

--- app/src/main/java/com/example/liveli/fragments/DiscoveryFragment.java
+++ app/src/main/java/com/example/liveli/fragments/DiscoveryFragment.java
@@ -2,17 +2,21 @@ package com.example.liveli.fragments;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -36,7 +40,7 @@ public class DiscoveryFragment extends Fragment {
 
     public static final String TAG = "MainActivity";
     public static final String SEARCH_URL =
-            "https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=25&order=viewCount&type=video&key=" + BuildConfig.YOUTUBE_KEY;
+            "https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=25&order=viewCount&type=video%s%skey=" + BuildConfig.YOUTUBE_KEY;
     public static final String VIDEO_URL =
             "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=liveStreamingDetails&%skey=" + BuildConfig.YOUTUBE_KEY;
     public static final String CHANNEL_URL =
@@ -46,6 +50,10 @@ public class DiscoveryFragment extends Fragment {
     List<String> views;
     List<String> channels;
     HashMap<String, String> hmap;
+    String category;
+    String query;
+    EditText etInput;
+    Button btnSearch;
 
     public DiscoveryFragment() {
         // Required empty public constructor
@@ -61,10 +69,11 @@ public class DiscoveryFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         RecyclerView rvStreams = view.findViewById(R.id.rvStreams);
+
         streams = new ArrayList<>();
-        views = new ArrayList<>();
-        channels = new ArrayList<>();
-        hmap = new HashMap<String, String>();
+        category = "&";
+        query = "&";
+
 
         StreamAdapter streamAdapter = new StreamAdapter(getContext(), streams);
 
@@ -72,8 +81,75 @@ public class DiscoveryFragment extends Fragment {
 
         rvStreams.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        etInput = view.findViewById(R.id.etInput);
+        btnSearch = view.findViewById(R.id.btnSearch);
+
+        Spinner dropdown = view.findViewById(R.id.sDropdown);
+        String[] items = new String[]{"Popular", "Music", "Gaming", "Sports", "News"};
+        ArrayAdapter<String> sAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(sAdapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                switch (i) {
+                    case 0:
+                        category = "&";
+                        break;
+                    case 1:
+                        category = "&videoCategoryId=10&";
+                        break;
+                    case 2:
+                        category = "&videoCategoryId=20&";
+                        break;
+                    case 3:
+                        category = "&videoCategoryId=17&";
+                        break;
+                    case 4:
+                        category = "&videoCategoryId=25&";
+                        break;
+                }
+                Log.i(TAG, "category is " + category);
+                streams = new ArrayList<>();
+                StreamAdapter streamAdapter = new StreamAdapter(getContext(), streams);
+                rvStreams.setAdapter(streamAdapter);
+                rvStreams.setLayoutManager(new LinearLayoutManager(getContext()));
+                retrieveData(streams, streamAdapter, SEARCH_URL, category, query);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                category = "&";
+                retrieveData(streams, streamAdapter, SEARCH_URL, category, query);
+            }
+        });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etInput.getText().toString().isEmpty()) {
+                    query = "&";
+                }
+                else {
+                    query = "&q=" + etInput.getText().toString() + "&";
+                    streams = new ArrayList<>();
+                    StreamAdapter streamAdapter = new StreamAdapter(getContext(), streams);
+                    rvStreams.setAdapter(streamAdapter);
+                    rvStreams.setLayoutManager(new LinearLayoutManager(getContext()));
+                    retrieveData(streams, streamAdapter, SEARCH_URL, category, query);
+                }
+            }
+        });
+
+    }
+
+    public void retrieveData(List<Stream> streams, StreamAdapter streamAdapter, String SEARCH_URL, String category, String query){
+        views = new ArrayList<>();
+        channels = new ArrayList<>();
+        hmap = new HashMap<String, String>();
+
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get(SEARCH_URL, new JsonHttpResponseHandler()  {
+        client.get(String.format(SEARCH_URL, category, query), new JsonHttpResponseHandler()  {
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onSuccess(int statusCode, okhttp3.Headers headers, JSON json) {
@@ -165,6 +241,7 @@ public class DiscoveryFragment extends Fragment {
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
                 Log.d(TAG, "onFailure");
+                Log.i(TAG, "URL" + SEARCH_URL);
             }
         });
     }
