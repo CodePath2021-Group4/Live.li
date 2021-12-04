70a59d828ee26f138a6a1a8d660debf02bc4e669
julesy <julesssie.dev@gmail.com>
2021-12-04 16:16:49 -0500

    Feat - Personal feed partially implemented

--- app/src/main/java/com/example/liveli/parseobjects/UserProfile.java
+++ app/src/main/java/com/example/liveli/parseobjects/UserProfile.java
@@ -5,10 +5,13 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+
 @ParseClassName("UserProfile")
 public class UserProfile extends ParseObject {
     public static final String KEY_USER = "user";
     public static final String KEY_IMAGE = "profile_image";
+    public static final String KEY_FOLLOWED = "channels_followed";
 
     public ParseUser getUser(){ return getParseUser(KEY_USER); }
     public void setUser(ParseUser user){ put(KEY_USER, user); }
@@ -18,5 +21,9 @@ public class UserProfile extends ParseObject {
     }
     public void setImage(ParseFile image) {
         put(KEY_IMAGE, image);
+    }
+
+    public JSONArray getFollowed(){
+        return getJSONArray(KEY_FOLLOWED);
     }
 }
